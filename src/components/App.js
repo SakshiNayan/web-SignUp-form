07d10b1e0ef8a5321721053c7291c11c0d504618
Sakshi Nayan <sakshi12princi@gmail.com>
2022-09-11 19:09:04 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import '../styles/App.css';
+import { Navigate } from "react-router-dom"
 //import UserPg from "./UserPg";
 const App = () => {
   // const [emailError, setEmailError] = useState('')
@@ -31,7 +32,7 @@ const App = () => {
         setmandatoryerr(true);
     }
     const format = /[!@#$%^&*0-9()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-    if (!format.test(userData.username)) {
+    if (!format.test(userData.userName)) {
         setnameerr(true);
     }
 
@@ -45,11 +46,11 @@ const App = () => {
         setpassworderr(true);
     }
 
-    if (!userData.number) {
+    if (!userData.phoneNum) {
         setnumbererr(true)
     }
-    let mynumber = "" + userData.number
-    if (mynumber.length !== 10) {
+    let mynumber = "" + userData.phoneNum
+    if (mynumber.length === 10) {
 
         setnumberlengtherr(true)
     }
@@ -58,88 +59,117 @@ const App = () => {
         setShow(!show)
 
     }
+  
 
 };
 
-  return (
-    <div id="main">
-      <form>
-            <h1>SignUp form</h1>
-            <hr></hr>
-            <div id="user-name" className="content-div">
-                <label className="lable">Name: </label>
-                <input className="input-box" type="alphanumeric" onChange={(e) => [
-                            setUserdata({ ...userData, username: e.target.value }),
-                            setmandatoryerr(false),
-                            setnameerr(false),
-                        ]}></input>
-                {nameerr && (
-                        <p style={{ color: "red" }}>
-                            {" "}
-                            Name is not alphanumeric!
-                        </p>
-                    )}
+return (
+    <>
+        
+
+        {!show && <div class="card">
+
+            <div class="card-body">
+                <h1 class="card-title" style={{  textAlign:"center"  }}>Hello {userData.email.split("@")[0]}</h1>
+                <p class="card-text" style={{textAlign: "center"}}>Form validation is successfull</p>
+                <a href="/#" class="btn btn-primary" style={{textAlign: "center"}} onClick={() => [setShow(!show), setUserdata({
+                    username: "",
+                    email: "",
+                    gender: "",
+                    number: "",
+                    password: ""
+
+                })]}>Go to form page</a>
             </div>
+        </div>}
+
+
+
+
+
+        {show && <div id='main' style={{ width: "500px", height: "650px" }}>
+            <h1>SignUp Form</h1>
+            <hr></hr>
+            {mandatoryerr && (
+                <p style={{ color: "red" }}> All fields are mandatory !</p>
+            )}
+            {/* <div id ='main'> */}
+                <div id="user-name" className="content-div">
+                    <label className="lable">Name: </label>
+                    <input className="input-box" type="alphanumeric" onChange={(e) => [
+                                setUserdata({ ...userData, username: e.target.value }),
+                                setmandatoryerr(false),
+                                setnameerr(false),
+                            ]}></input>
+                    {nameerr && (
+                            <p style={{ color: "red" }}>
+                                {" "}
+                                Name is not alphanumeric!
+                            </p>
+                        )}
+                </div>
             <div id="user-email" className="content-div">
                 <label className="lable">Email: </label>
                 <input className="input-box" type="email"
-                onChange={(e) => [
-                  setUserdata({ ...userData, email: e.target.value }),
-                  setmandatoryerr(false),
-                  setemailerr(false),
-              ]}
-              />
-              {emailerr && <p style={{ color: "red" }}>email must contain @ !</p>}
-            </div>
-            <div id="user-email" className="content-div">
-                <label className="lable">Gender: </label>
-                <select onChange={(e) => [
-                        setUserdata({ ...userData, gender: e.target.value }),
-                        setmandatoryerr(false),
-                        setgendererr(false),
-                    ]}>
-                    <option value=""></option>
-                    <option value="female">Female</option>
-                    <option value="male">Male</option>
-                    <option value="others">Others</option>
-                </select>
-                {gendererr && (
-                    <p style={{ color: "red" }}>
-                        {" "}
-                        Please identify as male, female or others !
-                    </p>
-                )}
-            </div>
-            <div id="user-num" className="content-div">
-                <label className="lable">Phone Number: </label>
-                <input className="input-box" type="numberic"  onChange={(e) => [
-                            setUserdata({ ...userData, number: parseInt(e.target.value) }),
-                            setmandatoryerr(false),
-                            setnumbererr(false),
-                            setnumberlengtherr(false)
-                        ]}></input>
-                {numbererr && <p style={{ color: "red" }}>Phone Number must contain numbers!</p>}
-                    {numberlengtherr && <p style={{ color: "red" }}>number length should be of 10 digits !</p>}
-                {/* <p className={numberValidation}>Please enter a valid number</p> */}
-            </div>
-            <div id="user-pass" className="content-div">
-                <label className="lable">Password: </label>
-                <input className="input-box" type="password"  
-                        
                         onChange={(e) => [
-                            setUserdata({ ...userData, password: e.target.value }),
+                        setUserdata({ ...userData, email: e.target.value }),
+                        setmandatoryerr(false),
+                        setemailerr(false),
+                    ]}
+                    />
+                {emailerr && <p style={{ color: "red" }}>email must contain @ !</p>}
+            </div>
+                <div id="user-email" className="content-div">
+                    <label className="lable">Gender: </label>
+                    <select onChange={(e) => [
+                            setUserdata({ ...userData, gender: e.target.value }),
                             setmandatoryerr(false),
-                            setpassworderr(false),
-                        ]}></input>
-                {passworderr && (
-                        <p style={{ color: "red" }}> Password must contain atleast 6 letters!</p>
+                            setgendererr(false),
+                        ]}>
+                        <option value=""></option>
+                        <option value="female">Female</option>
+                        <option value="male">Male</option>
+                        <option value="others">Others</option>
+                    </select>
+                    {gendererr && (
+                        <p style={{ color: "red" }}>
+                            {" "}
+                            Please identify as male, female or others !
+                        </p>
                     )}
-            </div>
-            <div id="submit-button" className="content-div">
-                <Link to=""><button  onClick={handleSubmit}>Submit</button></Link>
-            </div>
-        </form>
-    </div>
-  )
+                </div>
+                <div id="user-num" className="content-div">
+                    <label className="lable">Phone Number: </label>
+                    <input className="input-box" type="numberic"  onChange={(e) => [
+                                setUserdata({ ...userData, number: parseInt(e.target.value) }),
+                                setmandatoryerr(false),
+                                setnumbererr(false),
+                                setnumberlengtherr(false)
+                            ]}></input>
+                    {numbererr && <p style={{ color: "red" }}>Phone Number must contain numbers!</p>}
+                        {numberlengtherr && <p style={{ color: "red" }}>number length should be of 10 digits !</p>}
+                    {/* <p className={numberValidation}>Please enter a valid number</p> */}
+                </div>
+                <div id="user-pass" className="content-div">
+                    <label className="lable">Password: </label>
+                    <input className="input-box" type="password"  
+                            
+                            onChange={(e) => [
+                                setUserdata({ ...userData, password: e.target.value }),
+                                setmandatoryerr(false),
+                                setpassworderr(false),
+                            ]}></input>
+                    {passworderr && (
+                            <p style={{ color: "red" }}> Password must contain atleast 6 letters!</p>
+                        )}
+                </div>
+                <div id="submit-button" className="content-div">
+                    {/* <Link to="/user"><button  onClick={handleSubmit}>Submit</button></Link> */}
+                    <button  onClick={handleSubmit}>Submit</button>
+                </div>
+        </div>}
+    </>
+);
+  
 }
 export default App;
